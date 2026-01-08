348d66dadd5655c8399a3daa5e2ccb3b7dbc72ce
Anant <anantdiagarwal@gmail.com>
2026-01-08 08:48:43 +0530

    set things up to display surface

--- v2/script.js
+++ v2/script.js
@@ -13,9 +13,9 @@ const GRID_HEIGHT_CELLS = 100;
 
 // VISUAL RULES
 const CELL_SIZE_PX = 5;
-const SNAKE_COLOR = 'black';
-const FOOD_COLOR = 'red';
 const FRAME_DELAY = 60;
+const FOOD_COLOR = 'red';
+const SNAKE_COLOR = 'black';
 const CANVAS_BG_COLOR = 'yellow';
 
 /* 
@@ -24,21 +24,24 @@ const CANVAS_BG_COLOR = 'yellow';
     -> knows how to clear pixels
     -> talks directly to browser's api
     -> has zero idea about what the entities in the game are
-
-    * Is directly coupled with DrawingSurface class because CanvasJS needs to know height/width of the surface to boot up
 */
 class GraphicsEngine {
- constructor(drawingSurface){
+ constructor(surfaceDrawCommand){
     // setup the canvas
     this.canvas = document.getElementById('canvas');
     this.ctx = this.canvas.getContext("2d");
-    this.canvas.width = drawingSurface.width;
-    this.canvas.height = drawingSurface.height;
+    this.canvas.width = surfaceDrawCommand.width;
+    this.canvas.height = surfaceDrawCommand.height;
+    this.surface = surfaceDrawCommand;
  }
 
  drawRectangle(drawCommand) {
     this.ctx.fillStyle = drawCommand.color;
     this.ctx.fillRect(drawCommand.x, drawCommand.y, drawCommand.width, drawCommand.height);
+ }
+
+ clear(){
+    this.drawRectangle(this.surface);
  }
 }
 
@@ -48,17 +51,9 @@ class GraphicsEngine {
     comprehensible, it's just some rectangles that have to be drawn by the GraphicsEngine
 */
 class Renderer {
-    constructor(surface, snake, food){
-        this.surface = surface;
-        this.snake = snake;
-        this.food = food;
-    }
 
-    composeFrame(){
+    composeFrame(gameState){
         const drawCommands = [];
-
-        // game surface
-        drawCommands.push(new DrawCommand(0, 0, this.surface.height, this.surface.width, this.surface.bgcolor));
 
         // snake nodes
 
@@ -73,24 +68,13 @@ class Renderer {
     and consumed by the GraphicsEngine.
 */
 class DrawCommand {
-    constructor(x, y, height, width, color){
+    constructor(x, y, width, height, color){
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
         this.color = color;
     }
-}
-
-/* 
-    DrawingSurface - ENTITY : Background Surface in which the Game lies visually
-*/
-class DrawingSurface {
- constructor(height, width, bgcolor){
-    this.width = width;
-    this.height = height;
-    this.bgcolor = bgcolor;
- }
 }
 
 class SnakeNode {
@@ -114,8 +98,6 @@ class Snake {
 
 class Food {
     constructor(){
-        this.color = FOOD_COLOR;
-        this.size = FOOD_SIZE;
     }
 }
 
@@ -123,19 +105,61 @@ class InputHandler {
 
 }
 
-class GamePlay {
+class GameState {
+    constructor(snake, food){
+        this.snake = snake;
+        this.food = food;
+    }
+}
 
+class GameLoop {
+    constructor(gameState, graphicsEngine, renderer){
+        this.graphicsEngine = graphicsEngine;
+        this.gameState = gameState;
+        this.renderer = renderer;
+    }
+    
+    update(){
+    }
+
+    render(){
+        // clear the surface
+        this.graphicsEngine.clear();
+
+        const drawCommands = this.renderer.composeFrame(this.gameState);
+        for(let cmd of drawCommands){
+            this.graphicsEngine.drawRectangle(cmd);
+        }
+    }
+
+    tick(){
+        this.update();
+        this.render();
+    }
+
+    start(){
+        setInterval(() => this.tick(), FRAME_DELAY);
+    }
 }
 
 // Derived Values
+const CANVAS_X = 10;
+const CANVAS_Y = 10;
 const CANVAS_WIDTH = GRID_HEIGHT_CELLS * CELL_SIZE_PX;
 const CANVAS_HEIGHT = GRID_WIDTH_CELLS * CELL_SIZE_PX;
 
-// Define entities
+// INITIAL POSITIONS
+const SNAKE_INIT_X = (GRID_WIDTH_CELLS / 2) * CELL_SIZE_PX;
+const SNAKE_INIT_Y = (GRID_HEIGHT_CELLS / 2) * CELL_SIZE_PX;
+
+// Instantiate Game Entities
 const food = new Food();
 const snake = new Snake();
-const surface = new DrawingSurface(CANVAS_HEIGHT, CANVAS_WIDTH, CANVAS_BG_COLOR);
+const gameState = new GameState(snake, food);
+const surface = new DrawCommand(CANVAS_X, CANVAS_Y, CANVAS_WIDTH, CANVAS_HEIGHT, CANVAS_BG_COLOR);
 const graphicsEngine = new GraphicsEngine(surface);
+const renderer = new Renderer(surface);
+const gameLoop = new GameLoop(gameState, graphicsEngine, renderer);
 
-setInterval(() => {
-}, FRAME_DELAY)
+// start the game
+gameLoop.start()
